b1b6180388292fc3c94d41f0560f4c4e347a068d
Matthew Kelly <matthew.kelly2@gmail.com>
2022-08-20 16:15:11 -0400

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+pub struct Card {
+    index: i32,
+}
+
+impl Card {
+
+    /// Returns the suit of this [`Card`].
+    fn suit(&self) -> i32 {
+       self.index % 4
+    }
+
+}
+
+
+
+fn main() {
+    let card = Card{index: 9};
+    println!("Hello, world! Your card suit is: {}", card.suit());
+}
