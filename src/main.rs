0333b4f136ba50c1a62c130ca5a4663e0f1a5b18
Matthew Kelly <matthew.kelly2@gmail.com>
2022-08-20 16:19:53 -0400

    autoformat

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,13 @@ pub struct Card {
 }
 
 impl Card {
-
     /// Returns the suit of this [`Card`].
     fn suit(&self) -> i32 {
-       self.index % 4
+        self.index % 4
     }
-
 }
 
-
-
 fn main() {
-    let card = Card{index: 9};
+    let card = Card { index: 9 };
     println!("Hello, world! Your card suit is: {}", card.suit());
 }
