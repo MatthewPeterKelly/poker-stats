fed252236a7e8cfd17c2f28f8b444991d64a2ae5
Matthew Kelly <matthew.kelly2@gmail.com>
2022-08-21 06:19:15 -0400

    clean-up

--- src/main.rs
+++ src/main.rs
@@ -16,12 +16,16 @@ impl Card {
     fn index(&self) -> i32 {
         self.id / 4
     }
+
+    fn draw_random_card() -> Card {
+        Card {
+            id: rand::thread_rng().gen_range(0..51),
+        }
+    }
 }
 
 fn draw_five_cards() -> [Card; 5] {
-    let hand: [Card; 5] = array_init::array_init(|_| Card {
-        id: rand::thread_rng().gen_range(0..51),
-    });
+    let hand: [Card; 5] = array_init::array_init(|_| Card::draw_random_card());
     hand
 }
 
