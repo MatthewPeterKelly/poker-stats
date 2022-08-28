4f72c0b14d7e2eca76c715c9cf0b898c5a4b708d
Matthew Kelly <matthew.kelly2@gmail.com>
2022-08-28 13:29:22 -0400

    clean-up

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,11 @@ mod card;
 
 use crate::card::Card;
 
+/// Simple demo for the `poker-stats` crate. For now it
+/// does not support any arguments. It will do three things:
+/// (1) print out the cards in a sorted deck
+/// (2) print out a randomly drawn 5-card hand
+/// (3) print out a randomly drawn 7-card hand
 fn main() {
     let mut rng = rand::thread_rng();
 
