2b9a0806787bbfdea5ba1771b8517c74f6a37fd7
Matthew Kelly <matthew.kelly2@gmail.com>
2022-12-26 11:58:13 -0500

    Add 7 card score test

--- src/hand_score.rs
+++ src/hand_score.rs
@@ -200,5 +200,72 @@ mod tests {
         );
     }
 
-    // TODO:  add checks for 7-card hands
+    #[test]
+    fn seven_card_hand_scores() {
+        let deck = Deck::new();
+
+        check_hand_score_is_valid(
+            &deck,
+            &["5♣", "8♣", "3♣", "8♠", "7♣", "T♥", "9♦"],
+            HandScore {
+                pair: true,
+                ..Default::default()
+            },
+        );
+        check_hand_score_is_valid(
+            &deck,
+            &["5♣", "9♣", "8♣", "T♥", "9♦", "7♣", "2♣"],
+            HandScore {
+                flush: true,
+                pair: true,
+                ..Default::default()
+            },
+        );
+        check_hand_score_is_valid(
+            &deck,
+            &["5♣", "4♦", "7♣", "9♣", "8♣", "7♦", "4♥"],
+            HandScore {
+                pair: true,
+                two_pair: true,
+                ..Default::default()
+            },
+        );
+        check_hand_score_is_valid(
+            &deck,
+            &["5♣", "4♦", "7♣", "5♦", "9♣", "8♣", "5♥"],
+            HandScore {
+                pair: true,
+                three_of_a_kind: true,
+                ..Default::default()
+            },
+        );
+        check_hand_score_is_valid(
+            &deck,
+            &["5♦", "9♠", "7♠", "8♦", "6♥", "T♦", "J♥"],
+            HandScore {
+                straight: true,
+                ..Default::default()
+            },
+        );
+        check_hand_score_is_valid(
+            &deck,
+            &["4♦", "5♦", "8♦", "6♥", "5♣", "4♣", "5♥"],
+            HandScore {
+                pair: true,
+                three_of_a_kind: true,
+                full_house: true,
+                ..Default::default()
+            },
+        );
+        check_hand_score_is_valid(
+            &deck,
+            &["9♥", "7♥", "8♥", "6♥", "5♣", "T♥", "J♥"],
+            HandScore {
+                straight: true,
+                flush: true,
+                straight_flush: true,
+                ..Default::default()
+            },
+        );
+    }
 }
