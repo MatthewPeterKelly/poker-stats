9f938f7ca629f104f1ef0cc09855fd1760c51f48
spoloxs <googly4295@gmail.com>
2023-07-06 22:38:02 +0530

    Update: Changed <cli> commands

--- src/args.rs
+++ src/args.rs
@@ -1,29 +1,35 @@
-use clap:: {
-    Parser,
-};
+use clap::{Args, Parser, Subcommand};
 
 #[derive(Debug, Parser)]
 #[clap(author, version, about)]
-pub struct PokerArgs{
-    // Option command to run the program
-    #[arg(short = 'r', long = "run")]
-    /// Run program
-    pub run_option: bool,
-    
-    // Option command to display the deck of card
-    #[arg(short = 'd', long = "display")]
-    /// Run program
-    pub display_option: bool,
+pub struct PokerArgs {
+    #[command(subcommand)]
+    pub command: Option<ArgsCommands>,
 
-    // Optional argument to get the number of cards for playing
-    // Set to default so that if one doesn't any value it goes to demo mode
-    #[arg(required = false, default_value_t = 0, requires = "run_option")]
-    /// Number of cards.
-    pub cards_number: usize,
+    #[arg(long = "sorted-deck")]
+    pub sorted_deck: bool,
+}
 
-    // Optional argument to get the number of samples if required
-    // Set to default for demo mode
-    #[arg(required = false, default_value_t = 20_000)]
-    /// Number of samples.
-    pub samples_number: u32,
+#[derive(Debug, Subcommand)]
+pub enum ArgsCommands {
+    /// Draw Hands
+    DrawHands(DrawHand),
+
+    /// Get statistics
+    Statistics(GetStatistics),
+}
+
+#[derive(Debug, Args)]
+pub struct DrawHand {
+    /// Number of cards
+    pub hands_number: usize,
+}
+
+#[derive(Debug, Args)]
+pub struct GetStatistics {
+    /// Number of cards
+    pub hands_number: usize,
+
+    /// Number of samples
+    pub sample_number: u32,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,22 @@
 // Note: Each of the utility files must be added here as a
 // module so that it can be properly included in others.
 mod aggregate_score;
+mod args;
 mod card;
 mod deck;
 mod hand;
 mod hand_score;
 mod hand_stats;
-mod args;
 
-use poker_stats::aggregate_score::sample_aggregate_scores;
-use rand::rngs::ThreadRng;
+use crate::args::{ArgsCommands, DrawHand, PokerArgs};
 use crate::card::Card;
 use crate::hand::Hand;
 use crate::hand_score::HandScore;
 use crate::hand_stats::HandStats;
+use args::GetStatistics;
 use clap::Parser;
-use crate::args::PokerArgs;
+use poker_stats::aggregate_score::sample_aggregate_scores;
+use rand::rngs::ThreadRng;
 
 /// Simple demo for the `poker-stats` crate. For now it
 /// does not support any arguments. It will do three things:
@@ -25,56 +26,63 @@ use crate::args::PokerArgs;
 /// (3) compute and print the card stats
 fn main() {
     let args: PokerArgs = PokerArgs::parse();
-    let mut rng = rand::thread_rng();
-    if args.run_option
-    {
-        if args.cards_number == 5{
-            output_specific::<5>(rng, args.samples_number);
-        }
-        else if args.cards_number == 7{
-            output_specific::<7>(rng, args.samples_number);
-        }
-        else{
-            {
-                println!("");
-                let five_card_hand = Hand::<5>::draw(&mut rng);
-                println!("{five_card_hand}");
-                let hand_stats = HandStats::from(&five_card_hand);
-                println!("{hand_stats}");
-                let hand_score = HandScore::from(&hand_stats);
-                println!("{hand_score}");
 
-                println!("");
-                let seven_card_hand = Hand::<7>::draw(&mut rng);
-                println!("{seven_card_hand}");
-                let hand_stats = HandStats::from(&seven_card_hand);
-                println!("{hand_stats}");
-                let hand_score = HandScore::from(&hand_stats);
-                println!("{hand_score}");
+    // Matching the command
+    match &args.command {
+        // For draw-hand command
+        Some(ArgsCommands::DrawHands(DrawHand { hands_number })) => {
+            // If call draw-hands with hands_number as 5
+            if *hands_number == 5 {
+                output_specific::<5>(rand::thread_rng(), 20000);
+            }
+            // If call draw-hands with hands_number as 7
+            else if *hands_number == 7 {
+                output_specific::<7>(rand::thread_rng(), 20000);
+            }
+            // If hand_number is not 5 not 7
+            else {
+                println!("Invalid number. Enter 5 or 7")
+            }
+        }
 
-                // Now draw N random hands and check the stats!
-                println!("");
-                // Note: there is probably some way to deduce the type of the RNG here...
-                let scores = sample_aggregate_scores::<5, rand::rngs::ThreadRng>(&mut rng, 20_000);
-                println!("{scores}")
-            };
+        // For statistics command
+        Some(ArgsCommands::Statistics(GetStatistics {
+            hands_number,
+            sample_number,
+        })) => {
+            if *hands_number == 5 {
+                output_specific::<5>(rand::thread_rng(), *sample_number);
+            } else if *hands_number == 7 {
+                output_specific::<7>(rand::thread_rng(), *sample_number);
+            } else {
+                println!("Invalid number. Enter 5 or 7")
+            }
+        }
+
+        None => {
+            // If neither sorted_deck option is called nor any other i.e. for cargo run command (Default run)
+            if !(args.sorted_deck) {
+                output_specific::<5>(rand::thread_rng(), 20000);
+            }
         }
     }
 
-    println!("Sorted Deck:");
-    for id in 0..52 {
-        let card = Card { id };
-        if card.suit().id == 3 {
-            println!("  {}", card);
-        } else {
-            print!("  {}  ", card);
+    if args.sorted_deck {
+        println!("Sorted Deck:");
+
+        for id in 0..52 {
+            let card = Card { id };
+
+            if card.suit().id == 3 {
+                println!("  {}", card);
+            } else {
+                print!("  {}  ", card);
+            }
         }
     }
 }
 
-
-
-pub fn output_specific<const CARD_NUMBER: usize>(mut rng: ThreadRng, sample_number: u32){
+pub fn output_specific<const CARD_NUMBER: usize>(mut rng: ThreadRng, sample_number: u32) {
     println!("");
     let card_hand = Hand::<CARD_NUMBER>::draw(&mut rng);
     println!("{card_hand}");
